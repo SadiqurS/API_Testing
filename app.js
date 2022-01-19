5eb563e1f70d26d56cf30a5b014ce5578cca6cf7
Sadiqur Sakib <sadiqursakib42@gmail.com>
2022-01-19 17:49:07 -0500

    api

---
+++ app.js
@@ -0,0 +1,64 @@
+//using XML to retrive data from an API
+// const req = new XMLHttpRequest();
+
+// req.onload = function () {
+//     console.log("ALL DONE WITH REQUEST!!!")
+//     const data = JSON.parse(this.responseText);
+//     console.log(data.ticker.price);
+// }
+
+// req.onerror = function () {
+//     console.log("ERROR!!!")
+//     console.log(this);
+// }
+
+// req.open('GET', 'https://api.cryptonator.com/api/ticker/btc-usd')
+// req.send();
+
+
+//using fetch to retrive data from an API
+// fetch('https://api.cryptonator.com/api/ticker/btc-usd')
+//     .then(res => {
+//         console.log("RESPONSE, WAITING TO PARSE...", res)
+//         return res.json()
+//     })
+//     .then(data => {
+//         console.log("DATA PARSED...")
+//         console.log(data.ticker.price)
+//     })
+//     .catch(e => {
+//         console.log("OH NO! ERROR!", e)
+//     })
+
+
+//const fetchBitcoinPrice = async () => {
+//   try {
+//        const res = await fetch('https://api.cryptonator.com/api/ticker/btc-usd');
+//        const data = await res.json();
+//       console.log(data.ticker.price)
+//    } catch (e) {
+//        console.log("SOMETHING WENT WRONG!!!", e)
+//    }
+//}
+
+
+// using axios to retrive data
+// axios.get("https://api.cryptonator.com/api/ticker/btc-usd")
+//    .then(res =>{
+//        console.log(res.data.ticker.price)
+//     })
+//     .catch(err =>{
+//         console.log("error",err)
+//     })
+
+//using an async function and axios to retrive data from an api
+const btcPrice = async () => {
+    try{
+        const res = await axios.get("https://api.cryptonator.com/api/ticker/btc-usd")
+        console.log(res.data.ticker.price)
+    }
+    catch(e){
+        console.log("error",e)
+    }
+}
+btcPrice();
